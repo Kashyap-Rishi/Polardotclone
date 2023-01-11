1464647c02c93e8a026a12654be5ef8a4f115775
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-01-11 19:21:57 +0530

    Navbar commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './app.scss';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+
+import {useState} from "react"
+import "./navbar.scss"
+
+const Navbar = () => {
+ 
+    const[isScrolled, setIsScrolled]=useState(false);
+
+    window.onscroll=() =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return() => (window.onscroll==null);
+    }
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+    <div className="container">
+        <div className="left">
+            <img src="images/polardotimage-1.png" alt =""/>
+            
+
+           
+
+        </div>
+        <div className="right">
+            
+        <span>About Us</span>
+           
+            
+            
+           <span>Our Services</span>
+          
+           <span>Works</span>
+           <span>Gallery</span>
+           <span>FAQs</span>
+           
+           
+        
+        </div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default Navbar;
