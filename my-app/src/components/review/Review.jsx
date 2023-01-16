17a41bbac1e30cfe051207c37c801adf2741a189
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-01-16 20:17:33 +0530

    styledcrousel

--- my-app/src/components/review/Review.jsx
+++ my-app/src/components/review/Review.jsx
@@ -2,6 +2,36 @@ import React from "react";
 import "./review.scss";
 
 const Review = () => {
+
+    function animation1(){
+        document.getElementById('cir1').className='c1'
+        document.getElementById('cir2').className='circle2'
+        document.getElementById('cir3').className='circle3'
+        document.getElementById('cir4').className='circle4'
+        
+    }
+
+    function animation2(){
+        document.getElementById('cir1').className='circle1'
+        document.getElementById('cir2').className='c2'
+        document.getElementById('cir3').className='circle3'
+        document.getElementById('cir4').className='circle4'
+        
+    }
+    function animation3(){
+        document.getElementById('cir1').className='circle1'
+        document.getElementById('cir2').className='circle2'
+        document.getElementById('cir3').className='c3'
+        document.getElementById('cir4').className='circle4'
+        
+    }
+    function animation4(){
+        document.getElementById('cir1').className='circle1'
+        document.getElementById('cir2').className='circle2'
+        document.getElementById('cir3').className='circle3'
+        document.getElementById('cir4').className='c4'
+        
+    }
     return (
         <div className="reviewSection">
             <div className="reviewHead">
@@ -9,58 +39,21 @@ const Review = () => {
             </div>
             <div className="carousel">
                 <div className="firstrev">
-                    "Love that each component is handmadeand customised. Great work!"
+                    <div className="reviewone"><i>"Love that each component is handmadeand customised. Great work!"</i></div>
                     <br>
                     </br>
-                    <br>
-                    </br>
-                    <font>Shawarma Tales <a href="">@shawarmatales</a></font>
-                </div>
-                <div className="secondrev">
-
-                    "Impressed with master class support of the team and
-                    really look forward for the future.Really, really
-                    well made! Love that each component is handmade and
-                    customised. Great Work!"
-
-                    <br>
-                    </br>
-                    <br>
-                    </br>
-                    <font>Tommy Reaves <a href="">@shawarmatales</a></font>
-                </div>
-                <div className="thirdrev">
-                    "Impressed with master class support of the team and
-                    really look forward for the future.Really, really
-                    well made! Love that each component is handmade and
-                    customised. Great Work!"
-
-                    <br>
-                    </br>
-                    <br>
-                    </br>
-                    <font>Tommy Reaves <a href="">@shawarmatales</a></font>
-                </div>
-                <div className="forthtrev">
-                    "Impressed with master class support of the team and
-                    really look forward for the future.Really, really
-                    well made! Love that each component is handmade and
-                    customised. Great Work!"
-
-                    <br>
-                    </br>
-                    <br>
-                    </br>
-                    <font>Tommy Reaves <a href="">@shawarmatales</a></font>
+                    <font size="16px" className="reviewer">Shawarma Tales <a href="" className="link">@shawarmatales</a></font>
+                    <div className="subtext">Restraunts and takeaways</div>
                 </div>
 
-            </div>
-            <div className="fourcircles">
-                    <div className="c1"><img src="images/02.png"></img></div>
-                    <div className="c2"><img src="images/02.png"></img></div>
-                    <div className="c3"><img src="images/02.png"></img></div>
-                    <div className="c4"><img src="images/02.png"></img></div>
+        </div>
+        <div className="fourcircles">
+                    <div className="circle1" id="cir1"><img src="images/02.png" onClick={animation1} ></img></div>
+                    <div className="circle2" id="cir2"><img src="images/02.png" onClick={animation2}></img></div>
+                    <div className="circle3" id="cir3"><img src="images/02.png" onClick={animation3}></img></div>
+                    <div className="circle4" id="cir4"><img src="images/02.png" onClick={animation4}></img></div>
                 </div>
+
         </div>
     )
 }
