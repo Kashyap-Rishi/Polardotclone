dbcf1d4aabeb1bcf313778c560300180ba236d80
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-01-20 00:21:50 +0530

    footer relayed section

---
+++ my-app/src/components/connect/Connect.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './connect.scss'
+
+const Connect = () => {
+  return (
+    <div className="background">
+    <div className="connection">
+    <h2>Ready to Work?</h2>
+    <p>Want to start a Project? Branding, Website or Franchise, lets create something awesome together Connect on WhatsApp.</p>
+    <a>Start a project</a>
+    </div>
+    </div>
+  )
+}
+
+export default Connect
