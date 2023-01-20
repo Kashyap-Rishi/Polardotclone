e47ab9dacafdd943b128d92a3b639a72f90890d3
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-01-20 17:29:58 +0530

    carouselmadeadhura

--- my-app/src/components/review/Review.jsx
+++ my-app/src/components/review/Review.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./review.scss";
 
 const Review = () => {
+
+    const [review,fillreview]=useState("")
+    const [reviewer,fillreviewer]=useState("")
+    const [link,fillink]=useState("")
+    const [subtext,fillsubtext]=useState("")
+
+    function fillreview1(){
+        fillreview("Love that each component is handmade customised. Great work!")
+        fillreviewer("Shawarma Tales")
+        fillink("@shawarmatales")
+        fillsubtext("Restaurant/Takeaways")
+    }
+    function fillreview2(){
+        fillreview("Impressed with master class support of the team and really look forward for the future. Really,really well made! Love that each component is handmade and customised. Great Work!")
+        fillreviewer("Tommy Reaves")
+        fillink("@shawarmatales")
+        fillsubtext("CEO Founder")
+    }
+    function fillreview3(){
+        fillreview("Impressed with master class support of the team and really look forward for the future. Really,really well made! Love that each component is handmade and customised. Great Work!")
+        fillreviewer("Tommy Reaves")
+        fillink("@shawarmatales")
+        fillsubtext("Restaurant/Takeaways")
+    }
+    function fillreview4(){
+        fillreview("Impressed with master class support of the team and really look forward for the future. Really,really well made! Love that each component is handmade and customised. Great Work!")
+        fillreviewer("Tommy Reaves")
+        fillink("@shawarmatales")
+        fillsubtext("CEO Founder")
+    }
+
+
+
+    
 
     function animation1(){
         document.getElementById('cir1').className='c1'
@@ -39,19 +73,21 @@ const Review = () => {
             </div>
             <div className="carousel">
                 <div className="firstrev">
-                    <div className="reviewone"><i>"Love that each component is handmadeand customised. Great work!"</i></div>
+                    <div className="reviewone"><i>{review}</i></div>
                     <br>
                     </br>
-                    <font size="16px" className="reviewer">Shawarma Tales <a href="" className="link">@shawarmatales</a></font>
-                    <div className="subtext">Restraunts and takeaways</div>
+                    <font size="16px" className="reviewer">{reviewer} <a href="" className="link">{link}</a></font>
+                    <div className="subtext">{subtext}</div>
                 </div>
+
+                
 
         </div>
         <div className="fourcircles">
-                    <div className="circle1" id="cir1"><img src="images/02.png" onClick={animation1} ></img></div>
-                    <div className="circle2" id="cir2"><img src="images/02.png" onClick={animation2}></img></div>
-                    <div className="circle3" id="cir3"><img src="images/02.png" onClick={animation3}></img></div>
-                    <div className="circle4" id="cir4"><img src="images/02.png" onClick={animation4}></img></div>
+                    <div className="circle1" id="cir1" onClick={fillreview1}><img src="images/02.png" onClick={animation1} ></img></div>
+                    <div className="circle2" id="cir2" onClick={fillreview2}><img src="images/02.png" onClick={animation2}></img></div>
+                    <div className="circle3" id="cir3" onClick={fillreview3}><img src="images/02.png" onClick={animation3}></img></div>
+                    <div className="circle4" id="cir4" onClick={fillreview4}><img src="images/02.png" onClick={animation4}></img></div>
                 </div>
 
         </div>
