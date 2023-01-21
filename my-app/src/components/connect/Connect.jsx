4e4050500c73173a0532beeeed1fdbb0c47dfeb6
MANGALAM RAJ <mangalamraj@MANGALAMs-MacBook-Air.local>
2023-01-21 16:27:41 +0530

    Faqsectionwork

--- my-app/src/components/connect/Connect.jsx
+++ my-app/src/components/connect/Connect.jsx
@@ -4,6 +4,15 @@ import './connect.scss'
 const Connect = () => {
   return (
     <div className="background">
+                    <div className="neechewala">
+                    <div className="loading">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
     <div className="connection">
     <h2>Ready to Work?</h2>
     <p>Want to start a Project? Branding, Website or Franchise, lets create something awesome together <b>Connect on WhatsApp.</b></p>
