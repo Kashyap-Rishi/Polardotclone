d7567fe284f43ae772c5dcf97a3415998323a92a
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-01-25 16:29:45 +0530

    STars updated

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -3,6 +3,10 @@ import './about.scss'
 const About = () => {
   return (
     <div className="about">
+        <div id="stars"></div>
+    <div id="stars2"></div>
+    <div id="stars3"></div>
+    <div id="stars4"></div>
 <div className ="left">
   <h1>We create  powerful Brand Experience.</h1>
   <p>We shape Brands & Businesses gain a competitive edge to help them grow and engage their audience to build brand awareness.</p>
